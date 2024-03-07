846b1a871f3bc074de59e26fdcefc1ee7fae450f
baraniclas <niclasdan@gmail.com>
2024-03-07 10:07:25 +0100

    Did Code.

--- gol_2d_vis.c
+++ gol_2d_vis.c
@@ -1,27 +1,44 @@
 #include <stdio.h>
+#include <SDL2/SDL.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
-
-int sz;
-int lim;
-int el, eu, fl, fu, time_steps;
-int **cells;
-int **neighbours;
 
 void print_array(int **matrix);
 void primordial_soup(int **matrix);
 void count_neighbours(int **matrix);
 void update();
 void reset_matrix(int ** matrix);
+void renderGrid(SDL_Renderer *renderer);
+
+#define SCREEN_WIDTH 1000
+#define SCREEN_HEIGHT 1000
+#define CELL_SIZE 25
+#define GRID_WIDTH 1000
+#define GRID_HEIGHT 1000
+
+int sz;
+int lim;
+int el, eu, fl, fu, time_steps;
+int **cells;
+int **neighbours;
+int grid[GRID_WIDTH][GRID_HEIGHT] = {0}; // Initialize grid
 
 int main() {
-    sz = 10; // set size of tensor in 3D
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0, &window, &renderer);
+
+
+    sz = 1000; // set size of tensor in 3D
     el = 2;     // environment lower 
     eu = 3;     // environment upper
     fl = 3;     // fertility lower
     fu = 3;     // fertility upper
-    time_steps = 15;
+    time_steps = 100;
     lim = 50;
 
     srand(4);       // set to 4 just to be consistent
@@ -46,11 +63,22 @@ int main() {
     printf("loops begin here \n");
 
     for (int i = 0; i < time_steps; i++){
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                SDL_Quit();
+                return 0;
+            }
+        }
         count_neighbours(cells);
         update();
         reset_matrix(neighbours);
-        print_array(cells);
-        printf("\n");
+        // print_array(cells);
+        // printf("\n");
+
+        renderGrid(renderer);
+
+        SDL_Delay(100); // Adjust speed of simulation
     }
 
     
@@ -119,6 +147,23 @@ void reset_matrix(int **matrix) {
     }
 }
 
-// change matrix
+void renderGrid(SDL_Renderer *renderer) {
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // White background
+    SDL_RenderClear(renderer);
+
+    // Render each cell
+    for (int x = 0; x < GRID_WIDTH; x++) {
+        for (int y = 0; y < GRID_HEIGHT; y++) {
+            if (cells[x][y] == 1) {
+                SDL_Rect cellRect = {x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE};
+                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Black cell
+                SDL_RenderFillRect(renderer, &cellRect);
+            }
+        }
+    }
+
+    SDL_RenderPresent(renderer);
+}
+
 
 // https://www.digitalocean.com/community/tutorials/hash-table-in-c-plus-plus hashtables
